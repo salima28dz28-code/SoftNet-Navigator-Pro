4792c6825d122ecf4f8f31357b1f3208632a7942
SoftNet <softnet@example.com>
2026-07-18 12:28:39 +0200

    fix: Add material-icons-extended to fix build errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     
     // MapLibre Native integration
     implementation("org.maplibre.gl:android-sdk:10.2.0")
